40094f98233cccce5a663123915be76935a13901
Ed <ed@polarity.io>
2023-05-27 22:45:27 -0400

    Fix state management of toggles

--- components/block.js
+++ components/block.js
@@ -31,18 +31,39 @@ polarity.export = PolarityComponent.extend({
     this.set('activeTab', defaultKey);
     this.set('currentDisplayedData', this.get(defaultKey + 'Data'));
 
-    const currentData = this.get('currentDisplayedData');
-
-    if (currentData.Passed.length <= 3) {
-      this.set('passed', true);
-    }
-
-    if (currentData.Failed.length <= 3) {
-      this.set('failed', true);
-    }
-
-    if (currentData.Warnings.length <= 3) {
-      this.set('warnings', true);
+    if (!this.get('block._state')) {
+      // init section states that we track to ensure that state is unique per tab
+      // and is not lost if the details block is closed.
+      this.set('block._state', {
+        mx: {
+          passed: false,
+          failed: false,
+          warnings: false,
+          information: false
+        },
+        blacklist: {
+          passed: false,
+          failed: false,
+          warnings: false,
+          information: false
+        },
+        http: {
+          passed: false,
+          failed: false,
+          warnings: false,
+          information: false
+        },
+        https: {
+          passed: false,
+          failed: false,
+          warnings: false,
+          information: false
+        }
+      });
+      this.setTabDefaultState('mx');
+      this.setTabDefaultState('blacklist');
+      this.setTabDefaultState('http');
+      this.setTabDefaultState('https');
     }
 
     this._super(...arguments);
@@ -51,20 +72,28 @@ polarity.export = PolarityComponent.extend({
     changeTab: function (tabName) {
       this.set('activeTab', tabName);
       this.set('currentDisplayedData', this.get(tabName + 'Data'));
+      console.info(this.get('currentDisplayedData'));
     },
-    togglePassedResults: function () {
-      this.toggleProperty('passed');
-    },
-    toggleFailedResults: function () {
-      this.toggleProperty('failed');
-    },
-    toggleWarningResults: function () {
-      this.toggleProperty('warnings');
+    toggleSection: function (sectionName) {
+      const activeTab = this.get('activeTab');
+      this.toggleProperty(`block._state.${activeTab}.${sectionName}`);
     },
     toggleQuota: function () {
       this.getQuota();
       this.toggleProperty('showQuota');
     }
+  },
+  setTabDefaultState: function (tabName) {
+    const currentData = this.get(`${tabName}Data`);
+    if (currentData) {
+      this.setTabState(tabName, 'passed', currentData.Passed.length <= 3);
+      this.setTabState(tabName, 'failed', currentData.Failed.length <= 3);
+      this.setTabState(tabName, 'warnings', currentData.Warnings.length <= 3);
+      this.setTabState(tabName, 'information', currentData.Information.length <= 3);
+    }
+  },
+  setTabState(tabName, sectionName, isOpen) {
+    this.set(`block._state.${tabName}.${sectionName}`, isOpen);
   },
   getQuota: function () {
     if (!this.get('quotaRequested')) {
